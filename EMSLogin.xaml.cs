9c76ea26f1c906d382bcfe194c19c9f7767c4f85
Micah Gonzales <micahcharishg@yahoo.com>
2024-01-10 17:52:47 +0800

    Added the login system for EMS

---
+++ EMSLogin.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinalTerm_Project_EMS
+{
+    /// <summary>
+    /// Interaction logic for EMS_Login.xaml
+    /// </summary>
+    public partial class EMS_Login : Window
+    {
+
+        EmployeeDatabaseDataContext db = new EmployeeDatabaseDataContext(Properties.Settings.Default.MockEMSDatabaseConnectionString);
+
+        public EMS_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            string password = tbx_password.Password, email = tbx_email.Text;
+            List<USP_LOGIN_EMPLOYEEResult> accounts = new List<USP_LOGIN_EMPLOYEEResult>();
+
+            accounts = db.USP_LOGIN_EMPLOYEE(email, password).ToList();
+
+            if (accounts.Count == 1)
+            {
+                if (accounts[0].Status == "ACTIVE")
+                {
+                    if (accounts[0].PositionName == "ADMIN")
+                    {
+                        if (accounts[0].DepartmentName == "HR")
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+
+            }
+
+        }
+    }
+
+}
+    
+
