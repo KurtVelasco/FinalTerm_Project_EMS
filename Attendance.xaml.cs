e1733b19e8abba34187191623694d5b344030099
keaneatl <KeaneAndre.Tolentino@studentambassadors.com>
2024-01-11 14:08:17 +0800

    Create Attendance Management UI

---
+++ Attendance.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinalTerm_Project_EMS
+{
+    /// <summary>
+    /// Interaction logic for Attendance.xaml
+    /// </summary>
+    public partial class Attendance : Window
+    {
+        public EmployeeDatabaseDataContext DB { get; set; } = new EmployeeDatabaseDataContext(Properties.Settings.Default.MockEMSDatabaseConnectionString);
+
+        public Attendance()
+        {
+            InitializeComponent();
+        }
+    }
+}
